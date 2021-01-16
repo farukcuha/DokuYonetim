6cf043e64c38a9e92ba92dd307eeb36812a84172
farukcuha <ahmetfarukcuha@hotmail.com>
2021-01-16 10:13:41 +0300

    added activities

---
+++ app/src/main/java/com/example/dokuyonetim/Activities/TamamlananSiparisAyrinti.java
@@ -0,0 +1,14 @@
+package com.example.dokuyonetim.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TamamlananSiparisAyrinti extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tamamlanan_siparis_ayrinti);
+    }
+}
